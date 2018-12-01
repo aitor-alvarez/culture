6a34be989cd37b7b290c59411ec6b2173a93cbec
aitor-mir-nlp <aitoraalvarez@gmail.com>
2018-11-30 15:01:54 -1000

    initial commit

---
+++ static/assets/js/ajax.js
@@ -0,0 +1,37 @@
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+    //CSRF token
+    function csrfSafeMethod(method) {
+            // these HTTP methods do not require CSRF protection
+            return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+        }
+    //Ajax calls
+    function saveResponse(answer_id, response){
+        $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+            }
+        }
+        });
+        $.ajax({
+            type: 'POST',
+            url: '/save_response/'+answer_id+'/'+response
+        })
+        .done(function( data ) {
+                //alert('SAVED!!!');
+            });
+    }
