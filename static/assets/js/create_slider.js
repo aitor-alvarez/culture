f7a40a7a7b4d5d7dc1ea16100fa2ab94acd10c5f
aitor-mir <aitoraalvarez@gmail.com>
2019-02-08 10:01:01 -1000

    changes in  GUI

--- static/assets/js/create_slider.js
+++ static/assets/js/create_slider.js
@@ -2,18 +2,21 @@ function create_slider(item){
            var  class1 = 'slider-experts-' + item[0];
            var class2 = 'slider-results-' + item[2];
             $('#results').append(
-                '                        <div class="row s12">\n' +
-                '                                 <p >' + item[4] + '</p></div>\n' +
-                '                        </div></br>\n' +
-                 '                         <div class="row s12"><strong>You</strong>\n' +
-                '                                <div class="' + class2 + '"></div></br></br>\n' +
-                '                        </div>\n' +
-                '                        <div class="row s12"><strong>Expert</strong>\n' +
-                '                                <div class="' + class1 + '"></div></br></br>\n' +
-                '                    </div>\n' +
-                '                        <div class="row s12">\n' +
-                '                                 <p>' + item[5] + '</p></div>\n' +
-                '</br></br></br></br>');
+                '                        <div class="col s12">\n' +
+                '                         <ul>\n' +
+                '                        <li class="collection-item">\n' +
+                '                                 <p >' + item[4] + '</p></li>\n' +
+                 '                         <li class="collection-item"><strong>You</strong>\n' +
+                '                                <div class="' + class2 + '"></div>\n' +
+                '                        </li></br></br>\n' +
+                '                        <li class="collection-item"><strong>Expert</strong>\n' +
+                '                                <div class="' + class1 + '"></div>\n' +
+                '                        </li>\n' +
+                '                        <li class="collection-item">\n' +
+                '                                 <p class="feedback">' + item[5] + '</p>' +
+                '                       </li>\n' +
+                '</ul></div>'+
+                '</br>');
             $('.' + class1).attr('id', item[0]);
             $('.' + class2).attr('id', item[2]);
             var s1 = document.getElementById(item[0]);
@@ -36,7 +39,7 @@ function create_slider(item){
                 decimals: 1
             })
         });
-        var pipFormats = {'0':'Strongly disagree', '5':'Strongly agree'};
+       // var pipFormats = {'0':'Strongly disagree', '5':'Strongly agree'};
         noUiSlider.create(s2, {
             start: item[3],
             connect: true,
@@ -49,12 +52,12 @@ function create_slider(item){
                 'max': 5
             },
             pips: {
-                mode: 'range',
-                format: {
+                mode: 'range'
+                /*format: {
                     to: function(a){
                     return pipFormats[a];
                         }
-                    }
+                    }*/
             }
         });
         s2.setAttribute('disabled', true);
